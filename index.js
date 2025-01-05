48db95836e7676028542bf7a304a63cff64386c1
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-01-05 20:55:52 +0600

    user delete api and store api create

--- index.js
+++ index.js
@@ -46,9 +46,20 @@ async function run() {
       const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
-    // user related api
+    // get all users
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    // user post related api
     app.post("/user", async (req, res) => {
       const user = req.body;
+      // insert email if user does'nt exists
+      const query = { user_email: user.user_email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ massage: "user already exists", insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
@@ -63,6 +74,13 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // user delete api
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
